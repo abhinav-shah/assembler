f88254d1b7e51965795bca7471ea5a7c90a1345c
Abhinav Shah <abhinav.shah@gmail.com>
2015-06-09 20:32:48 -0700

    configured logging

--- assembler/src/main/java/org/abhishah/mvc/controllers/HelloWorldController.java
+++ assembler/src/main/java/org/abhishah/mvc/controllers/HelloWorldController.java
@@ -1,14 +1,19 @@
 package org.abhishah.mvc.controllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/index")
 public class HelloWorldController {
+	
+	private static final Logger LOG = LogManager.getLogger();
 
   @RequestMapping()
   public String helloWorld() {
+	  LOG.debug("Inside hello world controller");
     return "index";
   }
 }
